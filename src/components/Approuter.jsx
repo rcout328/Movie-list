6612b48a6c23a783ee89fe3fda3bc28b06e96e65
rcout328 <112163924+rcout328@users.noreply.github.com>
2024-02-01 02:48:23 +0530

    Update Approuter.jsx

--- src/components/Approuter.jsx
+++ src/components/Approuter.jsx
@@ -5,7 +5,7 @@ import Tv from "./Tv";
 import Maintv from "./Maintv";
 import Voice from "./Voice";
 import Search from "./Search";
-import Kk from "./Kk1"
+import Kk from "./Kk"
 import Search1 from "./Search1";
 import Kk2 from "./Kk2";
 
