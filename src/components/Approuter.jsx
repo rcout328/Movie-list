05d496b7dd3e2d210948cf8002118938cda65d58
rcout328 <112163924+rcout328@users.noreply.github.com>
2024-02-01 23:55:19 +0530

    Update Approuter.jsx

--- src/components/Approuter.jsx
+++ src/components/Approuter.jsx
@@ -47,10 +47,6 @@ const Approuter = () => {
           path: '/kk2/:_id',
           element: <Kk2/>
       },
-      {
-        path: '/network',
-        element: <Network/>
-    },
         
     ]);
 
